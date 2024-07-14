ef1265fbc324adda6251ccb87986d789d5919c9f
dazanez <dasl.672004@gmail.com>
2024-07-13 20:12:34 -0500

    File moved to correct folder (bottom nav bar to widgets/shared)

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,4 +1,4 @@
 export 'shared/custom_appbar.dart';
-export 'movies/custom_buttom_navigationbar.dart';
+export 'shared/custom_buttom_navigationbar.dart';
 export 'movies/movies_slideshow.dart';
 export 'movies/movies_horizontal_listview.dart';
